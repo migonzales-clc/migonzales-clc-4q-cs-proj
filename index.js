e8efb005871655eabbcbc8a66bf6b8303d9ff295
migonzales-clc <77388945+migonzales-clc@users.noreply.github.com>
2021-05-29 23:46:00 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,288 @@
+var custorder = [];
+function plantFunction1(){
+    document.getElementById("overlaypp1").style.display = "block";
+}
+function plantFunction2(){
+    document.getElementById("overlaypp2").style.display = "block";
+}
+function plantFunction3(){
+    document.getElementById("overlaypp3").style.display = "block";
+}
+function plantFunction4(){
+    document.getElementById("overlaypp4").style.display = "block";
+}
+function plantFunction5(){
+    document.getElementById("overlaypp5").style.display = "block";
+}
+function plantFunction6(){
+    document.getElementById("overlaypp6").style.display = "block";
+}
+function plantFunction7(){
+    document.getElementById("overlaypp7").style.display = "block";
+}
+function plantFunction8(){
+    document.getElementById("overlaypp8").style.display = "block";
+}
+function plantFunction9(){
+    document.getElementById("overlaypp9").style.display = "block";
+}
+function plantFunction10(){
+    document.getElementById("overlaypp10").style.display = "block";
+}
+function plantFunction11(){
+    document.getElementById("overlaypp11").style.display = "block";
+}
+function plantFunction12(){
+    document.getElementById("overlaypp12").style.display = "block";
+}
+function plantFunction13(){
+    document.getElementById("overlaypp13").style.display = "block";
+}
+function plantFunction14(){
+    document.getElementById("overlaypp14").style.display = "block";
+}
+function plantFunction15(){
+    document.getElementById("overlaypp15").style.display = "block";
+}
+function plantFunction16(){
+    document.getElementById("overlaypp16").style.display = "block";
+}
+function plantFunction17(){
+    document.getElementById("overlaypp17").style.display = "block";
+}
+function plantFunction18(){
+    document.getElementById("overlaypp18").style.display = "block";
+}
+function plantFunction19(){
+    document.getElementById("overlaypp19").style.display = "block";
+}
+function plantFunction20(){
+    document.getElementById("overlaypp20").style.display = "block";
+}
+function plantFunction21(){
+    document.getElementById("overlaypp21").style.display = "block";
+}
+function plantFunction22(){
+    document.getElementById("overlaypp22").style.display = "block";
+}
+function plantFunction23(){
+    document.getElementById("overlaypp23").style.display = "block";
+}
+function plantFunction24(){
+    document.getElementById("overlaypp24").style.display = "block";
+}
+function aboutusFunction(){
+    document.getElementById("aboutus").style.display = "block";
+}
+function returnFunction(){
+    document.getElementById("overlaypp1").style.display = "none";
+    document.getElementById("overlaypp2").style.display = "none";
+    document.getElementById("overlaypp3").style.display = "none";
+    document.getElementById("overlaypp4").style.display = "none";
+    document.getElementById("overlaypp5").style.display = "none";
+    document.getElementById("overlaypp6").style.display = "none";
+    document.getElementById("overlaypp7").style.display = "none";
+    document.getElementById("overlaypp8").style.display = "none";
+    document.getElementById("overlaypp9").style.display = "none";
+    document.getElementById("overlaypp10").style.display = "none";
+    document.getElementById("overlaypp11").style.display = "none";
+    document.getElementById("overlaypp12").style.display = "none";
+    document.getElementById("overlaypp13").style.display = "none";
+    document.getElementById("overlaypp14").style.display = "none";
+    document.getElementById("overlaypp15").style.display = "none";
+    document.getElementById("overlaypp16").style.display = "none";
+    document.getElementById("overlaypp17").style.display = "none";
+    document.getElementById("overlaypp18").style.display = "none";
+    document.getElementById("overlaypp19").style.display = "none";
+    document.getElementById("overlaypp20").style.display = "none";
+    document.getElementById("overlaypp21").style.display = "none";
+    document.getElementById("overlaypp22").style.display = "none";
+    document.getElementById("overlaypp23").style.display = "none";
+    document.getElementById("overlaypp24").style.display = "none";
+    document.getElementById("aboutus").style.display = "none";
+}
+function aloFunction(){
+    document.getElementById("returnbut").style.display = "block";
+    document.getElementById("anthucontain").style.display = "none";
+    document.getElementById("aglocontain").style.display = "none";
+    document.getElementById("philocontain").style.display = "none";
+}
+function anthuFunction(){
+    document.getElementById("returnbut").style.display = "block";
+    document.getElementById("alocontain").style.display = "none";
+    document.getElementById("aglocontain").style.display = "none";
+    document.getElementById("philocontain").style.display = "none";
+}
+function aglaoFunction(){
+    document.getElementById("returnbut").style.display = "block";
+    document.getElementById("alocontain").style.display = "none";
+    document.getElementById("anthucontain").style.display = "none";
+    document.getElementById("philocontain").style.display = "none";
+}
+function philoFunction(){
+    document.getElementById("returnbut").style.display = "block";
+    document.getElementById("alocontain").style.display = "none";
+    document.getElementById("anthucontain").style.display = "none";
+    document.getElementById("aglocontain").style.display = "none";
+}
+function retFunction(){
+    document.getElementById("alocontain").style.display = "block";
+    document.getElementById("anthucontain").style.display = "block";
+    document.getElementById("aglocontain").style.display = "block";
+    document.getElementById("philocontain").style.display = "block";
+    document.getElementById("returnbut").style.display = "none";
+}
+function addcartFunction1(){
+    alert("Product was Added Successfully");
+    var obj = {"prod_name":"Alocasia micholitziana","prod_price":299.99};
+    custorder.push(obj);
+    obj = {};
+}
+function addcartFunction2(){
+    alert("Product was Added Successfully");
+    var obj = {"prod_name":"Alocasia amazonica","prod_price":599.99};
+    custorder.push(obj);
+    obj = {};
+}
+function addcartFunction3(){
+    alert("Product was Added Successfully");
+    var obj = {"prod_name":"Alocasia zebrina","prod_price":249.99};
+    custorder.push(obj);
+    obj = {};
+}
+function addcartFunction4(){
+    alert("Product was Added Successfully");
+    var obj = {"prod_name":"Alocasia reginula","prod_price":549.99};
+    custorder.push(obj);
+    obj = {};
+}
+function addcartFunction5(){
+    alert("Product was Added Successfully");
+    var obj = {"prod_name":"Alocasia bambino","prod_price":449.99};
+    custorder.push(obj);
+    obj = {};
+}
+function addcartFunction6(){
+    alert("Product was Added Successfully");
+    var obj = {"prod_name":"Alocasia silver dragon","prod_price":3249.99};
+    custorder.push(obj);
+    obj = {};
+}
+function addcartFunction7(){
+    alert("Product was Added Successfully");
+    var obj = {"prod_name":"Anthurium magnificum","prod_price":1499.99};
+    custorder.push(obj);
+    obj = {};
+}
+function addcartFunction8(){
+    alert("Product was Added Successfully");
+    var obj = {"prod_name":"Anthurium crystallinum","prod_price":1999.99};
+    custorder.push(obj);
+    obj = {};
+}
+function addcartFunction9(){
+    alert("Product was Added Successfully");
+    var obj = {"prod_name":"Anthurium regale","prod_price":24999.99};
+    custorder.push(obj);
+    obj = {};
+}
+function addcartFunction10(){
+    alert("Product was Added Successfully");
+    var obj = {"prod_name":"Anthurium luxurians","prod_price":11999.99};
+    custorder.push(obj);
+    obj = {};
+}
+function addcartFunction11(){
+    alert("Product was Added Successfully");
+    var obj = {"prod_name":"Anthurium warocqueanum","prod_price":19999.99};
+    custorder.push(obj);
+    obj = {};
+}
+function addcartFunction12(){
+    alert("Product was Added Successfully");
+    var obj = {"prod_name":"Anthurium veitchii","prod_price":19999.99};
+    custorder.push(obj);
+    obj = {};
+}
+function addcartFunction13(){
+    alert("Product was Added Successfully");
+    var obj = {"prod_name":"Aglaonema silver bay","prod_price":349.99};
+    custorder.push(obj);
+    obj = {};
+}
+function addcartFunction14(){
+    alert("Product was Added Successfully");
+    var obj = {"prod_name":"Aglaonema red lipstick","prod_price":299.99};
+    custorder.push(obj);
+    obj = {};
+}
+function addcartFunction15(){
+    alert("Product was Added Successfully");
+    var obj = {"prod_name":"Aglaonema snow white","prod_price":399.99};
+    custorder.push(obj);
+    obj = {};
+}
+function addcartFunction16(){
+    alert("Product was Added Successfully");
+    var obj = {"prod_name":"Aglaonema pink moon","prod_price":249.99};
+    custorder.push(obj);
+    obj = {};
+}
+function addcartFunction17(){
+    alert("Product was Added Successfully");
+    var obj = {"prod_name":"Aglaonema super red","prod_price":599.99};
+    custorder.push(obj);
+    obj = {};
+}
+function addcartFunction18(){
+    alert("Product was Added Successfully");
+    var obj = {"prod_name":"Aglaonema rising star","prod_price":449.99};
+    custorder.push(obj);
+    obj = {};
+}
+function addcartFunction19(){
+    alert("Product was Added Successfully");
+    var obj = {"prod_name":"Philodendron gloriosum","prod_price":2499.99};
+    custorder.push(obj);
+    obj = {};
+}
+function addcartFunction20(){
+    alert("Product was Added Successfully");
+    var obj = {"prod_name":"Philodendron melanochrysum","prod_price":3499.99};
+    custorder.push(obj);
+    obj = {};
+}
+function addcartFunction21(){
+    alert("Product was Added Successfully");
+    var obj = {"prod_name":"Philodendron verrucosum","prod_price":7499.99};
+    custorder.push(obj);
+    obj = {};
+}
+function addcartFunction22(){
+    alert("Product was Added Successfully");
+    var obj = {"prod_name":"Philodendron prince of orange","prod_price":5499.99};
+    custorder.push(obj);
+    obj = {};
+}
+function addcartFunction23(){
+    alert("Product was Added Successfully");
+    var obj = {"prod_name":"Philodendron dark lord","prod_price":5499.99};
+    custorder.push(obj);
+    obj = {};
+}
+function addcartFunction24(){
+    alert("Product was Added Successfully");
+    var obj = {"prod_name":"Philodendron pink princess","prod_price":12999.99};
+    custorder.push(obj);
+    obj = {};
+}
+function toStringFunction(){
+    var products = JSON.stringify(custorder);
+    document.getElementById("custorder").value = products;
+}
+function cartshowFunction(){
+    document.getElementById("checkouttable").style.display= "block";
+}
+function dissolveFunction(){
+    document.getElementById("checkouttable").style.display="none";
+}
